79fe9e89a45b9f87b57d31561216fa99d9909989
Sasidhar Varada <76954020+sasidharvarada@users.noreply.github.com>
2024-10-21 00:06:37 +0530

    Add files via upload

---
+++ indoor_code/constants.h
@@ -0,0 +1,183 @@
+// WiFi Configuration
+// const char* WIFI_SSID = "JioFi3_259D0E";  // WiFi SSID
+// const char* WIFI_PASSWORD = "k61m12m0u5"; // WiFi Password
+
+const char* WIFI_SSID = "esw-m19@iiith";  // WiFi SSID
+const char* WIFI_PASSWORD = "e5W-eMai@3!20hOct"; // WiFi Password
+
+
+#define CSE_IP			  "dev-onem2m.iiit.ac.in"
+#define OM2M_ORGIN    "Tue_20_12_22:Tue_20_12_22"
+
+// #define CSE_IP			  "onem2m.iiit.ac.in"  // onem2m
+// #define OM2M_ORGIN    "AQSRMon@20:psX9MSnnrvyH"
+#define CSE_PORT    443
+#define HTTPS     false
+#define OM2M_MN     "/~/in-cse/in-name/"
+#define OM2M_AE     "AE-SR/SR-AQ"
+
+
+// #define node_1  //VN90-00 //library              1 
+// #define node_2//TH04-00    t-hub 5th floor    2
+// #define node_3//SN00-00 air-iot lab           6
+#define node_4//KN00-00//water-IOT lab        5
+// #define node_5//KH95-01 lab _3
+// #define node_6//VN01-00 sachin sir lab
+
+#ifdef node_1
+
+  #define CHANNEL_ID 2657305
+  #define CHANNEL_API_KEY "Z8JPQP9Z110MJJCA"  
+
+  #define OM2M_Node_ID "SR-AQ-VN90-00"
+  #define OM2M_DATA_CONT  "SR-AQ-VN90-00/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-VN90-00\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.0;
+  float c_pm2 = 1.0;
+  float m_pm10 = 1.0;
+  float c_pm10 = 1.0;
+
+  float m_db = 1.0;
+  float c_db = 1.0;
+
+#endif
+
+#ifdef node_2
+
+  #define CHANNEL_ID 2657592
+  #define CHANNEL_API_KEY "8C8CH9B2JUNZWYU6"
+
+  #define OM2M_Node_ID "SR-AQ-TH04-00"
+  #define OM2M_DATA_CONT  "SR-AQ-TH04-00/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-TH04-00\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.0;
+  float c_pm2 = 1.0;
+  float m_pm10 = 1.0;
+  float c_pm10 = 1.0;
+
+  float m_db = 1.0;
+  float c_db = 1.0;
+
+#endif
+
+#ifdef node_3
+  #define CHANNEL_ID 2657617
+  #define CHANNEL_API_KEY "88KR7A5K4ASO4RE5"
+
+  #define OM2M_Node_ID "SR-AQ-SN00-00"
+  #define OM2M_DATA_CONT  "SR-AQ-SN00-00/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-SN00-00\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.0;
+  float c_pm2 = 1.0;
+  float m_pm10 = 1.0;
+  float c_pm10 = 1.0;
+
+  float m_db = 1.0;
+  float c_db = 1.0;
+
+#endif
+
+#ifdef node_4
+
+  #define CHANNEL_ID 2657616
+  #define CHANNEL_API_KEY "CXI1Z1HP0VVDKIG7"
+
+  #define OM2M_Node_ID "SR-AQ-KN00-00"
+  #define OM2M_DATA_CONT  "SR-AQ-KN00-00/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-KN00-00\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.06805859;
+  float c_pm2 = 1.421629131241616;
+  float m_pm10 = 0.74048506;
+  float c_pm10 = 21.169480930789746;
+
+  float m_db = 1.0;
+  float c_db = 1.0;
+
+#endif
+
+#ifdef node_5
+
+  #define CHANNEL_ID  1591608
+  #define CHANNEL_API_KEY "MGYZNCH5TAPELMYO"
+
+  #define OM2M_Node_ID "SR-AQ-KH95-01"
+  #define OM2M_DATA_CONT  "SR-AQ-KH95-01/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-KH95-01\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.0;
+  float c_pm2 = 1.0;
+  float m_pm10 = 1.0;
+  float c_pm10 = 1.0;
+
+#endif
+
+#ifdef node_6
+
+  #define CHANNEL_ID  1591611
+  #define CHANNEL_API_KEY "Z3GHUG9HGXHVYKFY"
+
+  #define OM2M_Node_ID "SR-AQ-VN01-00"
+  #define OM2M_DATA_CONT  "SR-AQ-VN01-00/Data"
+  #define OM2M_DATA_LBL "[\"AE-SR-AQ\", \"SR-AQ-VN01-00\", \"V3.1.0\", \"SR-AQ-V3.1.0\"]"
+
+  float m_pm2 = 1.0;
+  float c_pm2 = 1.0;
+  float m_pm10 = 1.0;
+  float c_pm10 = 1.0;
+
+  float m_db = 1.0;
+  float c_db = 1.0;
+
+#endif
+
+unsigned long prev_time = 0;              // Time tracking variable for PM readings
+
+// Time tracking variables
+unsigned long previousMillis = 0;
+
+// WiFi client for posting data
+WiFiClient client;
+HTTPClient http;
+
+int code;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "time.google.com");
+ESP32Time rtc(0);
+
+#define MIN_VALID_TIME 1672531200
+#define MAX_VALID_TIME 2082844799
+
+static uint64_t ntp_epoch_time = 0;
+
+// Sound Sensor Configuration
+#define SoundSensorPin 36                 // GPIO pin for sound sensor
+#define VREF 5.0                          // Reference voltage for sound sensor
+float voltageValue, dbValue,dbValue_cal;              // Variables to store sensor readings
+
+// CO2 Sensor Configuration
+const int CO2_PIN = 13;                   // GPIO pin for CO2 sensor
+unsigned long duration, th, tl;           // Timing variables for CO2 reading
+int ppm;                                  // CO2 ppm value
+
+// AHT10 Sensor Configuration
+Adafruit_AHT10 aht;                       // AHT10 sensor object
+Adafruit_Sensor* aht_humidity;            // Humidity sensor object
+Adafruit_Sensor* aht_temp;                // Temperature sensor object
+float t;                                  // Temperature value
+float h;                                  // Humidity value
+
+// PM Sensor Configuration
+int pm2 = 0;                              // Variable to store PM2.5 value
+int pm10 = 0;    
+float pm2_cal=0;
+float pm10_cal=0;
+byte command_frame[9] = {0xAA, 0x02, 0x00, 0x00, 0x00, 0x00, 0x01, 0x67, 0xBB}; // PM command frame
+byte received_data[9];                    // Data received from PM sensor
+int sum = 0;                              // Checksum variable
+
+float aqi = 0.0;
---
+++ indoor_code/indoor_code.ino
@@ -0,0 +1,265 @@
+#include <ThingSpeak.h>          // For ThingSpeak communication
+#include <WiFi.h>                // For WiFi connectivity
+#include <WiFiUdp.h>             // For WiFi UDP
+#include <Adafruit_AHT10.h>      // For AHT10 sensor (Temperature and Humidity)
+#include <time.h>
+#include <NTPClient.h>
+#include <ESP32Time.h>
+#include <HTTPClient.h>
+#include "constants.h"
+
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting to WiFi");
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 30000) {
+    Serial.print('.');
+    delay(1000);
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected to " + String(WIFI_SSID) + " network");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Mac Address: ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("Signal strength (RSSI): ");
+    Serial.println(WiFi.RSSI());
+  } else {
+    Serial.println("\nFailed to connect to WiFi within 30 seconds. Restarting...");
+    ESP.restart();
+  }
+}
+
+void sync_time() {
+  static bool first_time = true;
+  int num_tries = 5;  // Number of attempts to get valid NTP time
+
+  // Check if the ESP32 is connected to WiFi
+  if (WiFi.status() == WL_CONNECTED) {
+    if (first_time == true) {
+      // Initialize NTPClient only once
+      timeClient.begin();
+      timeClient.setTimeOffset(0);
+      timeClient.update();
+      first_time = false;
+    }
+    // Configure the time zone (IST: Indian Standard Time, UTC+5:30)
+    configTime(5 * 3600, 30 * 60, "pool.ntp.org");
+    while (num_tries > 0) {
+      // Get the obtained NTP time
+      uint64_t ntp_time = timeClient.getEpochTime();
+      Serial.println(ntp_time);
+      // Validate the obtained NTP time
+      if (ntp_time != 0 && ntp_time >= MIN_VALID_TIME && ntp_time <= MAX_VALID_TIME) {
+        // Set the RTC time using the NTP time
+        rtc.setTime(ntp_time);
+        ntp_epoch_time = rtc.getEpoch();
+        Serial.println(ntp_epoch_time);
+        if (ntp_epoch_time < 1672000000) {
+          sync_time();
+        }
+        // Print the universal time and epoch time
+        Serial.print("Universal time: " + rtc.getDateTime(true) + "\t");
+        Serial.println(ntp_epoch_time, DEC); // For UTC, take timeoffset as 0
+        // Update the last_update time
+        prev_time = millis();
+        return;  // Exit the function after successful synchronization
+      } else {
+        // Invalid NTP time, reduce the number of remaining attempts
+        Serial.println("Invalid NTP time, retrying...");
+        num_tries--;
+        delay(1000);
+        Serial.println("All attempts failed, performing restarting time function ...");
+        ESP.restart();
+      }
+    }
+  }
+}
+
+void aht_setup() {
+  if (!aht.begin()) {
+    Serial.println("Failed to find AHT10 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("AHT10 Found!");
+  aht_temp = aht.getTemperatureSensor();
+  aht_humidity = aht.getHumiditySensor();
+}
+
+void aht_loop() {
+  sensors_event_t humidity;
+  sensors_event_t temp;
+  aht_humidity->getEvent(&humidity);
+  aht_temp->getEvent(&temp);
+  t = temp.temperature;
+  h = humidity.relative_humidity;
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" °C\t");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("% rH");
+  ThingSpeak.setField(1, t);
+  ThingSpeak.setField(2, h);
+}
+
+void PM_setup() {
+  sendCommand(0x01);
+  Serial.println("PM Found!");
+}
+
+void PM_Reading() {
+  if (millis() - prev_time > 5000) {
+    sendCommand(0x02);
+    prev_time = millis();
+  }
+  if (Serial.available()) {
+    Serial.readBytes(received_data, 9);
+    if (checksum()) {
+      calculatePM();
+    }
+  }
+}
+
+void sendCommand(byte command) {
+  command_frame[1] = command;
+  sum = command_frame[0] + command_frame[1] + command_frame[2] + command_frame[3] + command_frame[4] + command_frame[5] + command_frame[8];
+  int rem = sum % 256;
+  command_frame[6] = (sum - rem) / 256;
+  command_frame[7] = rem;
+  delay(1000);
+  Serial.write(command_frame, 9);
+}
+
+bool checksum() {
+  sum = int(received_data[0]) + int(received_data[1]) + int(received_data[2]) + int(received_data[3]) + int(received_data[4]) + int(received_data[5]) + int(received_data[8]);
+  return sum == ((int(received_data[6]) * 256) + int(received_data[7]));
+}
+
+void calculatePM() {
+  pm2 = int(received_data[4]) * 256 + int(received_data[5]);
+  delay(2000);
+  pm10 = int(received_data[2]) * 256 + int(received_data[3]);
+  Serial.print("PM2.5: ");
+  Serial.println(pm2);
+  Serial.print("PM10: ");
+  Serial.println(pm10);
+  pm2_cal = (m_pm2 * pm2) + c_pm2;
+  pm10_cal = (m_pm10 * pm10) + c_pm10;
+  Serial.print("PM2.5_cal: ");
+  Serial.println(pm2_cal);
+  Serial.print("PM10_cal: ");
+  Serial.println(pm10_cal);
+
+  if (pm2 < 2000) {
+    ThingSpeak.setField(3, pm2);
+    ThingSpeak.setField(4, pm2_cal);
+    ThingSpeak.setField(5, pm10);
+    ThingSpeak.setField(6, pm10_cal);
+  }
+}
+
+void CO2_Monitor() {
+  th = pulseIn(CO2_PIN, HIGH, 2008000) / 1000;
+  tl = 1004 - th;
+  ppm = 2000 * (th - 2) / (th + tl - 4);
+  Serial.print("CO2 Concentration: ");
+  Serial.println(ppm);
+  ThingSpeak.setField(7, ppm);
+}
+
+void sound_loop() {
+  voltageValue = analogRead(SoundSensorPin) / 4096.0 * VREF;
+  dbValue = voltageValue * 50.0;
+
+  Serial.print("Noise :");
+  Serial.print(dbValue, 1);
+  Serial.println(" dBA");
+
+  dbValue_cal = (m_db * (dbValue)) + c_db;
+  Serial.print(" Calibrated Noise level (dBA): ");
+  Serial.println(dbValue_cal);
+  
+  ThingSpeak.setField(8, dbValue);
+}
+
+void postToThingSpeak() {
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected ");
+  }
+  int x = ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_API_KEY);
+  ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_API_KEY);
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+}
+
+void postToOneM2M() {
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
+
+  String data = "[" + String(ntp_epoch_time) + " , " + String(pm2) + " , " + String(pm2_cal) + " , " + String(pm10) + " , " + String(pm10_cal) + " , " + String(t) + " , " + String(h) + " , " + String(ppm) + " , " + String(dbValue) +" , " + String(dbValue_cal) +" , " + String(aqi)+"]";
+
+  String server = "http://" + String(CSE_IP) + ":" + String(CSE_PORT) + String(OM2M_MN);
+
+  http.begin(server + OM2M_AE + "/" + OM2M_DATA_CONT + "/");
+
+  http.addHeader("X-M2M-Origin", OM2M_ORGIN);
+  http.addHeader("Content-Type", "application/json;ty=4");
+  http.addHeader("Content-Length", String(data.length()));
+
+  String req_data = "{\"m2m:cin\": {"
+                    "\"con\": \""
+                    + data + "\","
+                             "\"lbl\": "
+                    + OM2M_DATA_LBL + ","
+                                      "\"cnf\": \"text\""
+                                      "}}";
+  code = http.POST(req_data);
+  http.end();
+  Serial.println("OneM2M HTTP Response Code: " + String(code));
+}
+
+void setup() {
+  Serial.begin(9600);        // Initialize serial communication
+  initWiFi();                // Initialize WiFi connection
+  sync_time();
+  PM_setup();                // Initialize PM sensor
+  pinMode(CO2_PIN, INPUT);   // Set CO2 sensor pin as input
+  aht_setup();               // Initialize AHT10 sensor
+  pinMode(SoundSensorPin, INPUT);  // Set sound sensor pin as input
+  ThingSpeak.begin(client);
+  Serial.println("Starting ESP32...");
+  delay(3000);               // Delay for system stability
+}
+
+void loop() {
+  unsigned long currentMillis = millis();  // Get current time in milliseconds
+
+  // Reconnect to WiFi if not connected
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= 10000)) {
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+    Serial.println(" WiFi reconnected");
+    previousMillis = currentMillis;
+  }
+
+  // Call sensor monitoring functions
+  aht_loop();         // Read temperature and humidity
+  PM_Reading();       // Read PM2.5 and PM10 levels
+  CO2_Monitor();      // Read CO2 concentration
+  sound_loop();       // Read sound levels
+  //post the data
+  postToThingSpeak();
+  postToOneM2M();
+  delay(5000);
+}
