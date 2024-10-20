240c31ac48f718b88be1f20702903728b8a436cd
Sasidhar Varada <76954020+sasidharvarada@users.noreply.github.com>
2024-10-20 22:22:05 +0530

    Update esp_sound.ino

--- Basic_codes/esp_sound/esp_sound.ino
+++ Basic_codes/esp_sound/esp_sound.ino
@@ -1,24 +1,25 @@
-
-//esp32
-// #define SoundSensorPin 13  //this pin read the analog voltage from the sound level meter
-// #define VREF  5.0  //voltage on AREF pin,default:operating voltage
-
 //esp8266
-#define SoundSensorPin A0  // ESP8266 uses A0 for analog input
-#define VREF  3.3  // Voltage reference for ESP8266 (operating voltage)
+// #define SoundSensorPin A0  // ESP8266 uses A0 for analog input
+// #define VREF  3.3  // Voltage reference for ESP8266 (operating voltage)
 
-float voltageValue,dbValue;
+#define SoundSensorPin 36  // GPIO pin for sound sensor
+#define VREF 5.0           // Reference voltage for sound sensor
+float voltageValue, dbValue;
 
-void setup()
-{
-    Serial.begin(115200);
+void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+  pinMode(SoundSensorPin, INPUT);  // Set sound sensor pin as input
+  delay(3000);                     // Delay for system stability
 }
 
-void loop()
-{
-    voltageValue = analogRead(SoundSensorPin) / 1024.0 * VREF;
-    dbValue = voltageValue * 50.0;  //convert voltage to decibel value
-    Serial.print(dbValue,1);
-    Serial.println(" dBA");
-    delay(125);
+void loop() {
+  sound_loop();  // Read sound levels
+}
+
+void sound_loop() {
+  voltageValue = analogRead(SoundSensorPin) / 4096.0 * VREF;
+  dbValue = voltageValue * 50.0;
+  Serial.print("Sound Level: ");
+  Serial.print(dbValue, 1);
+  Serial.println(" dBA");
 }
